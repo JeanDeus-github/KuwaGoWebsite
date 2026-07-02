115af0b8b0b1fec35f18095d5031571288235628
Marcus <MarcusMalabag@live.com.ph>
2026-07-02 04:50:08 +0000

    Add Parallax

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,12 @@
-    const menu = document.querySelector('.menu');
+const menu = document.querySelector('.menu');
 
-    menu?.addEventListener('click', () => {
-      const isExpanded = menu.getAttribute('aria-expanded') === 'true';
-      menu.setAttribute('aria-expanded', `${!isExpanded}`);
-    });
+menu?.addEventListener('click', () => {
+  const isExpanded = menu.getAttribute('aria-expanded') === 'true';
+  menu.setAttribute('aria-expanded', `${!isExpanded}`);
+});
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header-background");
+  const offset = window.scrollY * 0.3;
+  header.style.backgroundPositionY = `-${offset}px`;
+});
